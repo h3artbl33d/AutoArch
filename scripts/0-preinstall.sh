f5b136079f92dddb9e37fc361cc41f082f6bc5ab
h3artbl33d <hello@h3artbl33d.nl>
2022-03-21 19:53:15 +0100

    Fix the LUKS partition (4)

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -54,7 +54,7 @@ sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 # create partitions
 sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
 sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
-sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
+sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'cryptroot' ${DISK} # partition 3 (Root), default start, remaining
 if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
     sgdisk -A 1:set:2 ${DISK}
 fi
@@ -112,7 +112,7 @@ echo -n "${LUKS_PASSWORD}" | cryptsetup -y --cipher aes-xts-plain64 --key-size 5
 # open luks container and ROOT will be place holder 
 echo -n "${LUKS_PASSWORD}" | cryptsetup open ${partition3} cryptroot -
 # now format that container
-mkfs.btrfs -L ROOT ${cryptroot}
+mkfs.btrfs -L cryptroot ${cryptroot}
 # create subvolumes for btrfs
 mount -t btrfs ${cryptroot} /mnt
 subvolumesetup
